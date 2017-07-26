9163d44dd1deaec0ab2c42bfaf8b9dab9a6e1791
Matt Erhart <mattjerhart@gmail.com>
2017-07-25 19:10:57 -0700

    reformated data to json file

---
+++ scripts/reformat_data.js
@@ -0,0 +1,33 @@
+var csv = require("fast-csv");
+var d3 = require("d3");
+var dataDir = "C:/Users/me/Google Drive/wormNet/wormNet/src/assets/data/";
+var jsonfile = require("jsonfile");
+
+function getCsvData(path) {
+  return new Promise((resolve, reject) => {
+    let data = [];
+    csv
+      .fromPath(path)
+      .on("data", function(row) {
+        data.push(row);
+      })
+      .on("end", () => {
+        resolve(data);
+      });
+  });
+}
+
+Promise.all([
+  getCsvData(dataDir + "277_positions.dat"),
+  getCsvData(dataDir + "spikeMat.csv")
+]).then(posSpikes => {
+  let pos = posSpikes[0];
+  let spikes = posSpikes[1];
+  let json = [];
+  pos.forEach((val, i) => {
+    json.push({ pos: val, spikes: spikes[i] });
+  });
+  jsonfile.writeFile(dataDir+'data.json', json, { spaces: 2 }, function(err) {
+    console.error(err);
+  });
+});
